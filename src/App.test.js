5a2c8bbf0a6e6510f6233f14012a08e7c8474f82
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 09:41:51 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';//react-rdeux
 import { store } from './app/store';
 import App from './App';
 
