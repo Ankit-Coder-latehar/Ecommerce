2001cda761fb43fec22060320c747044fa38dc58
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 09:15:33 +0530

    Update ProductForm.js

--- src/features/admin/components/ProductForm.js
+++ src/features/admin/components/ProductForm.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; //using react-redux
 import {
   clearSelectedProduct,
   createProductAsync,
