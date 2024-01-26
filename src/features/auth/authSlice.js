7fce6501ebc6371131f8ec916f59e7842da5d4e2
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 09:16:50 +0530

    Update authSlice.js

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'; //using reduxjs /toolkit
 import {
   loginUser,
   createUser,
