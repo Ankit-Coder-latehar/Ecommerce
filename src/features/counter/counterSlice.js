530a2596021b28840625f3882f15366fad95358b
Ankit-Coder-latehar <148073889+Ankit-Coder-latehar@users.noreply.github.com>
2024-01-26 09:26:00 +0530

    Update counterSlice.js

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './counterAPI'; //use of counter api
 
 const initialState = {
   value: 0,
